d5714fb92974be370cfd02f962b227f85a9b6564
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-07-06 17:06:01 -0300

    products already

--- src/components/ProductsItens/index.tsx
+++ src/components/ProductsItens/index.tsx
@@ -1,51 +1,58 @@
+import { useEffect } from 'react';
+import Script from 'next/script'
 import styles from './styles.module.scss'
 
 export function ProductsItems() {
+
+
     return (
         <div className={styles.container}>
+
             <div className={styles.productsContainer}>
-                <div className={styles.products}>
+                <div className='products'>
                     <h1>Off-White Red</h1>
                     <h3>AIR JORDAN 1 Retrô</h3>
 
                     <img src='./product1.png' alt="Air Jordan 1 retrô | Red off-white" />
                 </div>
 
-                <div className={styles.products}>
+                <div className='products'>
                     <h1>Off-White Red</h1>
                     <h3>AIR JORDAN 1 Retrô</h3>
 
                     <img src='./product2.png' alt="Air Jordan 1 retrô | Red off-white" />
                 </div>
 
-                <div className={styles.products}>
+                <div className='products'>
                     <h1>Red Off-White</h1>
                     <h3>AIR JORDAN 1 Retrô</h3>
 
                     <img src='./product3.png' alt="Air Jordan 1 retrô | Red off-white" />
                 </div>
 
-                <div className={styles.products}>
+                <div className='products'>
                     <h1>Orange Metalic</h1>
                     <h3>AIR JORDAN 1 Retrô</h3>
 
                     <img src='./product4.png' alt="Air Jordan 1 retrô | Red off-white" />
                 </div>
 
-                <div className={styles.products}>
+                <div className='products'>
                     <h1>Off-White Red</h1>
                     <h3>AIR JORDAN 1 Retrô</h3>
 
                     <img src='./product5.png' alt="Air Jordan 1 retrô | Red off-white" />
                 </div>
 
-                <div className={styles.products}>
+                <div className='products'>
                     <h1>Red Off-White</h1>
                     <h3>AIR JORDAN 1 Retrô</h3>
 
                     <img src='./product6.png' alt="Air Jordan 1 retrô | Red off-white" />
                 </div>
             </div>
+
+            <Script src="https://unpkg.com/scrollreveal" />
         </div>
     )
 }
